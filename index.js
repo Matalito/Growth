526421789ec20b16cd5f692602ba3c795b208b0a
Matalito <130173728+Matalito@users.noreply.github.com>
2023-04-20 23:04:23 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 let lastPressedX = '';
 let lastPressedY = '';
-let isPunching = false;
-let punchXY = '';
-let badGuy = '';
+//ctx.textAlign = "center";
+//ctx.textBaseline = "middle";
+ctx.font = "50px Trebuchet MS";
+let isPunching;
+let badGuy;
+let clickXY;
+let punchIntervalID;
+let immunity;
 
 window.addEventListener('resize', resizeCanvas);
 function resizeCanvas() {
@@ -27,32 +32,59 @@ function getRandomColor() {
     return color;
 }
 
+function moveAtoB(x1, y1, x2, y2, distance) {
+    const pointA = [x1, y1];
+    const pointB = [x2, y2];
+    const vectorAB = [pointB[0] - pointA[0], pointB[1] - pointA[1]];
+    const magnitudeAB = Math.sqrt(vectorAB[0] ** 2 + vectorAB[1] ** 2);
+    const directionVector = [vectorAB[0] * (distance / magnitudeAB), vectorAB[1] * (distance / magnitudeAB)];
+    newXY = [pointA[0] + directionVector[0], pointA[1] + directionVector[1]];
+    return newXY;
+}
+
 class Player {
     constructor() {
-        this.size = { x: 30, y: 30 }
-        this.color = getRandomColor();//'green';
+        this.size = { x: 30, y: 30 };
+        this.color = 'white';
         this.position = { x: 640, y: 360 };
-        this.velocity = 10
-        this.moveLeft = false
-        this.moveRight = false
-        this.moveUp = false
-        this.moveDown = false
+        this.velocity = 10;
+        this.healthMax = 100;
+        this.healthCurrent = 100;
+        this.damageCurrent = this.healthMax - this.healthCurrent;
+        this.damageSquareMax = 15;
+        this.damageSquareCurrent = this.damageSquareMax * (this.damageCurrent / this.healthMax);
+        this.moveLeft = false;
+        this.moveRight = false;
+        this.moveUp = false;
+        this.moveDown = false;
+        this.newXY = [];
         this.punchBox = {
-            left: punchXY[0] - 5,
-            right: punchXY[0] + 5,
-            top: punchXY[1] - 5,
-            bottom: punchXY[1] + 5
-        }
-        
+            left: this.newXY[0] - 10,
+            right: this.newXY[0] + 10,
+            top: this.newXY[1] - 10,
+            bottom: this.newXY[1] + 10
+        };
+
     }
 
     draw() {
         ctx.fillStyle = this.color;
         ctx.fillRect(this.position.x, this.position.y, this.size.y, this.size.x);
+        ctx.fillStyle = 'red';
+        ctx.fillRect(this.position.x + 15 - this.damageSquareCurrent, this.position.y + 15 - this.damageSquareCurrent, this.damageSquareCurrent * 2, this.damageSquareCurrent *2);
+        
+        if (damagePlayer()) {
+            this.healthCurrent -= badGuy.strength;
+            this.damageCurrent = this.healthMax - this.healthCurrent;
+            this.damageSquareCurrent = this.damageSquareMax * (this.damageCurrent / this.healthMax);
+            setTimeout(() => {
+                immunity = false;
+            }, 500)
+        }
     }
 
     punch() {
-        if (isPunching === true) {
+        if (isPunching) {
             ctx.fillStyle = 'red';
             ctx.beginPath();
             ctx.moveTo(this.position.x + 10, this.position.y + 10);
@@ -60,29 +92,29 @@ class Player {
             ctx.lineTo(this.position.x + 20, this.position.y + 20);
             ctx.lineTo(this.position.x + 10, this.position.y + 20);
             ctx.fill();
-            ctx.moveTo(punchXY[0] - 5, punchXY[1] - 5);
-            ctx.lineTo(punchXY[0] + 5, punchXY[1] - 5);
-            ctx.lineTo(punchXY[0] + 5, punchXY[1] + 5);
-            ctx.lineTo(punchXY[0] - 5, punchXY[1] + 5);
+            ctx.moveTo(this.newXY[0] - 5, this.newXY[1] - 5);
+            ctx.lineTo(this.newXY[0] + 5, this.newXY[1] - 5);
+            ctx.lineTo(this.newXY[0] + 5, this.newXY[1] + 5);
+            ctx.lineTo(this.newXY[0] - 5, this.newXY[1] + 5);
             ctx.fill();
             ctx.moveTo(this.position.x + 10, this.position.y + 10);
-            ctx.lineTo(punchXY[0] - 5, punchXY[1] + 5);
-            ctx.lineTo(punchXY[0] + 5, punchXY[1] + 5);
+            ctx.lineTo(this.newXY[0] - 5, this.newXY[1] + 5);
+            ctx.lineTo(this.newXY[0] + 5, this.newXY[1] + 5);
             ctx.lineTo(this.position.x + 20, this.position.y + 10);
             ctx.fill();
             ctx.moveTo(this.position.x + 20, this.position.y + 10);
-            ctx.lineTo(punchXY[0] - 5, punchXY[1] - 5);
-            ctx.lineTo(punchXY[0] - 5, punchXY[1] + 5);
+            ctx.lineTo(this.newXY[0] - 5, this.newXY[1] - 5);
+            ctx.lineTo(this.newXY[0] - 5, this.newXY[1] + 5);
             ctx.lineTo(this.position.x + 20, this.position.y + 20);
             ctx.fill();
             ctx.moveTo(this.position.x + 20, this.position.y + 20);
-            ctx.lineTo(punchXY[0] + 5, punchXY[1] - 5);
-            ctx.moveTo(punchXY[0] - 5, punchXY[1] - 5);
+            ctx.lineTo(this.newXY[0] + 5, this.newXY[1] - 5);
+            ctx.moveTo(this.newXY[0] - 5, this.newXY[1] - 5);
             ctx.lineTo(this.position.x + 10, this.position.y + 20);
             ctx.fill();
             ctx.moveTo(this.position.x + 10, this.position.y + 20);
-            ctx.lineTo(punchXY[0] + 5, punchXY[1] + 5);
-            ctx.lineTo(punchXY[0] + 5, punchXY[1] - 5);
+            ctx.lineTo(this.newXY[0] + 5, this.newXY[1] + 5);
+            ctx.lineTo(this.newXY[0] + 5, this.newXY[1] - 5);
             ctx.lineTo(this.position.x + 10, this.position.y + 10);
             ctx.fill();
         }
@@ -107,100 +139,114 @@ class Player {
 
     update() {
         pOne.draw();
-        this.punchBox.left = punchXY[0] - 5;
-        this.punchBox.right = punchXY[0] + 5;
-        this.punchBox.top = punchXY[1] - 5;
-        this.punchBox.bottom = punchXY[1] + 5;
-
+        this.punchBox = {
+            left: this.newXY[0] - 5,
+            right: this.newXY[0] + 5,
+            top: this.newXY[1] - 5,
+            bottom: this.newXY[1] + 5
+        }
+        pOne.punch();
         pOne.moveX();
         pOne.moveY();
-        pOne.punch();
     }
 }
 const pOne = new Player();
 
 class Baddie {
-    constructor({ size, health, color, velocity }) {
-        this.size = size
-        this.healthMax = health
-        this.healthCurrent = health
-        this.healthbarMax = 30
-        this.healthbarCurrent = 30 * (this.healthCurrent / this.healthMax)
-        this.color = color
-        this.position = { x: Math.random() * canvas.width, y: Math.random() * canvas.height }
-        this.velocity = velocity
+    constructor({ size, health, color, velocity, strength }) {
+        this.size = size;
+        this.healthMax = health;
+        this.healthCurrent = health;
+        this.healthbarMax = 30;
+        this.healthbarCurrent = 30 * (this.healthCurrent / this.healthMax);
+        this.color = color;
+        this.position = { x: Math.random() * canvas.width, y: Math.random() * canvas.height };
+        this.velocity = velocity;
+        this.newXY = [];
+        this.strength = strength;
     }
 
     draw() {
-        ctx.fillStyle = this.color
-        ctx.fillRect(this.position.x, this.position.y, this.size.x, this.size.y)
-
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.position.x, this.position.y, this.size.x, this.size.y);
+        ctx.fillStyle = 'red';
+        ctx.fillRect(this.position.x, this.position.y - 6, this.healthbarCurrent, 3);
         if (checkPunch()) {
-            this.healthbarCurrent = 30 * (this.healthCurrent / this.healthMax)
-            ctx.fillStyle = 'red';
-            ctx.fillRect(this.position.x, this.position.y - 6, this.healthbarCurrent, 3)
+            this.healthbarCurrent = 30 * (this.healthCurrent / this.healthMax);
         }
     }
 
     move() {
-        this.position
+        if (!checkPunch()) {
+            this.newXY = moveAtoB(this.position.x, this.position.y, pOne.position.x + 15, pOne.position.y + 15, this.velocity);
+            this.position.x = this.newXY[0];
+            this.position.y = this.newXY[1];
+        } else {
+            this.newXY = moveAtoB(this.position.x, this.position.y, clickXY[0], clickXY[1], 30);
+            this.position.x = this.newXY[0];
+            this.position.y = this.newXY[1];
+        }
     }
 
     update() {
-            badGuy.draw()
+        badGuy.draw();
+        badGuy.move();
     }
 }
 
 function genBaddies() {
     badGuy = new Baddie({
-        size: { x: 3 + Math.random() * 40, y: 3 + Math.random() * 40 },
+        size: { x: 10 + Math.random() * 40, y: 10 + Math.random() * 40 },
         health: 20 + Math.random() * 80,
         color: getRandomColor(),
-        velocity: 1 + Math.random() * 8
+        velocity: 1 + Math.random() * 5,
+        strength: 1 + Math.random() * 8
     })
     return badGuy
 }
 genBaddies()
 
 function checkBaddies() {
-    if (!badGuy) {
+    if (badGuy) {
+        badGuy.update();
+    } else {
         genBaddies();
     }
 }
 
-document.onkeydown = function (pressed) {
-    if (pressed.key === 'a') {
+document.onkeydown = function (e) {
+    if (e.key === 'a') {
         pOne.moveLeft = true;
         lastPressedX = 'a';
     }
-    if (pressed.key === 'd') {
+    if (e.key === 'd') {
         pOne.moveRight = true;
         lastPressedX = 'd';
     }
-    if (pressed.key === 'w') {
+    if (e.key === 'w') {
         pOne.moveUp = true;
         lastPressedY = 'w';
     }
-    if (pressed.key === 's') {
+    if (e.key === 's') {
         pOne.moveDown = true;
         lastPressedY = 's';
     }
 }
 
-document.onkeyup = function (pressed) {
-    if (pressed.key === 'a') {
+document.onkeyup = function (e) {
+    if (e.key === 'a') {
         pOne.moveLeft = false;
         lastPressedX = 'd';
     }
-    if (pressed.key === 'd') {
+    if (e.key === 'd') {
         pOne.moveRight = false;
         lastPressedX = 'a';
     }
-    if (pressed.key === 'w') {
+    if (e.key === 'w') {
         pOne.moveUp = false;
         lastPressedY = 's';
     }
-    if (pressed.key === 's') {
+    if (e.key === 's') {
         pOne.moveDown = false;
         lastPressedY = 'w';
     }
@@ -219,37 +265,61 @@ function checkPunch() {
 
 function damageBadGuy() {
     if (checkPunch()) {
-        badGuy.healthCurrent -= 5
+        badGuy.healthCurrent -= 5;
     }
     if (badGuy.healthCurrent <= 0) {
         badGuy = '';
     }
 }
 
-document.onmousedown = function (click) {
-    isPunching = true;
-    const pointB = [click.clientX, click.clientY];
-    const pointA = [pOne.position.x + 15, pOne.position.y + 15];
-    const vectorAB = [pointB[0] - pointA[0], pointB[1] - pointA[1]];
-    const magnitudeAB = Math.sqrt(vectorAB[0] ** 2 + vectorAB[1] ** 2);
-    const directionVector = [vectorAB[0] * (30 / magnitudeAB), vectorAB[1] * (30 / magnitudeAB)];
-    punchXY = [pointA[0] + directionVector[0], pointA[1] + directionVector[1]];
+function damagePlayer() {
+    if(badGuy && !immunity){
+    if (badGuy.position.x <= pOne.position.x + pOne.size.x &&
+        badGuy.position.x + badGuy.size.x >= pOne.position.x &&
+        badGuy.position.y <= pOne.position.y + pOne.size.y &&
+        badGuy.position.y + badGuy.size.y >= pOne.position.y) {
+            immunity = true;
+            return true;
+        }
+    }
+}
+
+function checkPunching() {
+    isPunching = true;  
+    pOne.newXY = moveAtoB(pOne.position.x + 15, pOne.position.y + 15, clickXY[0], clickXY[1], 30)
     checkPunch();
     damageBadGuy();
-    console.log(badGuy.healthCurrent);
     setTimeout(() => {
         isPunching = false;
-    }, 25)
+    }, 40)
+}
+
+document.onmousedown = function (e) {
+    clickXY = [e.clientX, e.clientY];
+    checkPunching();
+    punchIntervalID = setInterval(() => { 
+        document.onmousemove = function (f) {
+        clickXY = [f.clientX, f.clientY];}    
+        checkPunching()}, 150)
+}
+
+document.onmouseup = function() {
+    clearInterval(punchIntervalID);
 }
 
 function animate() {
+    if (pOne.healthCurrent > 0) {
     initialDraw();
     pOne.update();
-    if (badGuy) {
-    badGuy.update();    
-    }
     checkBaddies();
     window.requestAnimationFrame(animate);
+    } else {
+        ctx.fillStyle = 'black';
+        ctx.fillRect (canvas.width/4, canvas.height/4, canvas.width/2, canvas.height/2);
+        ctx.fillStyle = 'limegreen';        
+        ctx.font = "80px Trebuchet MS";
+        ctx.fillText('You Died Sucka!',canvas.width/4 + 50, canvas.height/2)
+    }
 }
 
 animate();
